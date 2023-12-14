08cc62549c372fc33c17c4417ebab31ebaf076ec
Hederson Boechat <hedersonboechat@gmail.com>
2023-12-14 10:32:34 -0300

    feat: changed the index page for both languages

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,7 +15,14 @@ export const languages = {
       'meta.title': 'Hederson Boechat - Solopreneur and Technologies',
       'button.read': 'Read more',
       'span.published': 'Published on',
-      'span.by': 'by'
+      'span.by': 'by',
+      'button.schedule': 'Schedule a call',
+      'span.about.title': 'Software Engineer, founder, and product builder',
+      'span.about.me': 'About me',
+      'span.about.blog': 'About this blog',
+      'span.about.description': 'I\'ve creating softwares since 2013, and I\'m currently working as a Software Engineer, but as my side hustle, I\'m creating my own products.',
+      'span.about.description2': 'This blog aims to be a platform for sharing all my experiences, whether related to what I\'m learning or doing. Currently, I\'m passionate about creating software products known as SaaS. You can expect content covering technical aspects as well as more business-focused topics such as marketing and sales.',
+      'span.latest.post': 'Latest posts',
     },
     'pt-br': {
       'nav.home': 'Home',
@@ -26,7 +33,14 @@ export const languages = {
       'meta.title': 'Hederson Boechat - Empreendedorismo e Tecnologias',
       'button.read': 'Leia mais',
       'span.published': 'Publicado em',
-      'span.by': 'por'
+      'span.by': 'por',
+      'button.schedule': 'Agende uma conversa',
+      'span.about.title': 'Software Engineer, founder, and product builder',
+      'span.about.me': 'Sobre mim',
+      'span.about.blog': 'Sobre este blog',
+      'span.about.description': 'Trabalho com a desenvolvimento de softwares desde 2013. Além disso, estou desenvolvendo meus próprios produtos com tudo que aprendi ao longo desses anos.',
+      'span.about.description2': 'Este blog tem como objetivo ser uma plataforma para compartilhar todas as minhas experiências, seja relacionado ao que estou aprendendo ou fazendo. Atualmente, tenho paixão por criar produtos de software como serviço tambem conhecido como SaaS. Aqui você irá encontrar conteúdo abordando aspectos técnicos, assim como tópicos mais voltados para o mundo dos negócios, como marketing e vendas.',
+      'span.latest.post': 'Ultimos posts',
     },
   } as const;
 
